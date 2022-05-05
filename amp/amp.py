ece6a963dd6c3c28f996983c2689cc16a44255ff
hczv <mchczv@gmail.com>
2022-05-05 22:25:09 +0200

    o

--- amp/amp.py
+++ amp/amp.py
@@ -130,7 +130,8 @@ class amp(commands.Cog):
         instances = await self.api_request(self.ADS_PORT, self.api_get_instances)
         self.ADS_INSTANCES = instances['result'][0]
 
-        x = self.ADS_INSTANCES.get('AvailableInstances').index(ID)
+        x = self.ADS_INSTANCES.get('AvailableInstances')
+        return type(x)
         status = await self.api_request(x['Port'], self.api_get_status)
         if status['State'] == 0:
             state = "OFF"
